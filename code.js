dbb453fd0ccc622e8742bcd31645afbce21f97ac
Ellie Q <qelliehuynh@gmail.com>
2025-02-11 12:40:12 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+/* console.log("Hello, world!"); */
+const string = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+;
+console.log(string)
+alert(string);
+/*The following code is used to create the vault combination addition equal 10 */
+const x = 5;
+const y = 5;
+console.log(x + y);
+const result1 = x + y;
+
+/*The following code is used to create the vault combination subtraciton to 40 */
+const a = 50;
+const b = 10;
+console.log(a - b);
+const result2 = a - b;
+
+
+/*The following code is used to create the vault combination multiplication equal 39 */
+const q = 13;
+const e = 3;
+console.log(q * e );
+const result3 = q * e;
+
+/*The following code is used to create the vault combination for result 1 , 2 and 3  */
+const tempLit = `The first digit is ${result1}, the second digit will be ${result2}, and the third digit will be ${result3}.`
+confirm(tempLit)
